19b8abf42f6fc55345e0d2efe5c7110a5f528ac2
darroyo606 <dcarroyop@gmail.com>
2023-09-16 09:33:40 -0500

    Primer Commit SP

---
+++ main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("https://www.us.despegar.com/flights/UIO/AGP?from=SB&di=1-0&reSearch=true")
+
+flights = driver.find_elements(By.CLASS_NAME, "cluster-container")
+for f in flights:
+    #airline_name=f.find.element(by=By.CLASS_NAME, value="airline-cotainer").text or "Combination"
+
+    airline_name =f.find_element(by=By.TAG_NAME, value="img").accessible_name
+    departure=f.find_element(by=By.CLASS_NAME, value="cluster-part-0").text
+    arrival = f.find_element(by=By.CLASS_NAME, value="cluster-part-1").text
+    day = f.find_element(by=By.CLASS_NAME, value="quantity-days").text
+    price = f.find_element(by=By.CLASS_NAME, value="price").text
+
+    print(airline_name)
+    print(departure)
+    print(arrival)
+    print(day)
+    print(price)
+
+#elem.clear()
+#elem.send_keys("pycon")
+#elem.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
+driver.close()
